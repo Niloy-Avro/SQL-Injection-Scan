bbf2f1af3964061da484e22b4a7041bf9f9202f3
Niloy Avro Mondal <niloyavromondal8@gmail.com>
2025-08-24 16:27:22 +0530

    Updated

--- sqli_scanner/settings.py
+++ sqli_scanner/settings.py
@@ -133,3 +133,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://sql-injection-scan-2.onrender.com",
+]
